7fac97ff42998d13fff34f9cb789dc16429bf333
Shosta <remilavedrine-git@yahoo.fr>
2013-04-12 17:10:05 +0200

    Add new MainWindow to handle TabBarController. Add "Training" ViewController.
    "Training" Viewcontroller does not ask the result of the memorization.

--- Memorizer/Memorizer/Classes/Controllers/QuestionSetsViewController/QuestionSetsViewController.h
+++ Memorizer/Memorizer/Classes/Controllers/QuestionSetsViewController/QuestionSetsViewController.h
@@ -1,19 +1,12 @@
-//
-//  FirstViewController.h
-//  Memorizer
-//
-//  Created by Rémi LAVEDRINE on 05/03/13.
-//  Copyright (c) 2013 Rémi Lavedrine. All rights reserved.
-//
 //--------------------------------------------------------
-// FT/OLNC/OLPS/SOFT/IDF-NANCY/MSF
+// FT/IMG/RD/MAPS/DVC/HOD
 //--------------------------------------------------------
 // Project     : Memorizer
-// File        : AppData.h
-// Created     : $ 25/04/12 $
+// File        : QuestionSetsViewController.h
+// Created     : $ 12/04/13 $
 // Maintainer  : $ Rémi LAVEDRINE $
 //
-// Copyright France Telecom 2004-2012, All Rights Reserved
+// Copyright Rémi Lavedrine 2004-2013, All Rights Reserved
 //
 // This software is the confidential and proprietary
 // information of France Telecom.
@@ -22,11 +15,12 @@
 // of the license agreement you entered into with
 // France Telecom.
 //--------------------------------------------------------
-//
+// 
 // @brief
+// 
 
-#import <UIKit/UIKit.h>
+___IMPORTHEADER_viewControllerSubclass___
 
-@interface FirstViewController : UIViewController
+@interface QuestionSetsViewController : UIViewController
 
 @end
