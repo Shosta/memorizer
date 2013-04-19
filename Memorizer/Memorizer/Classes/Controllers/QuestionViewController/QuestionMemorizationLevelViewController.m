80d05e8dab4eb01cb8a24b13cbbd4829012d462e
Shosta <remilavedrine-git@yahoo.fr>
2013-04-19 18:34:56 +0200

    Remove "next question" button on QuestionMemorizationLevelViewController

--- Memorizer/Memorizer/Classes/Controllers/QuestionViewController/QuestionMemorizationLevelViewController.m
+++ Memorizer/Memorizer/Classes/Controllers/QuestionViewController/QuestionMemorizationLevelViewController.m
@@ -21,12 +21,20 @@ static const int kDescriptionSection = 2;
 #pragma mark - Object
 
 - (id)initWithQuestionsArray:(NSMutableArray *)aQuestionsArray{
-  self = [super initWithNibName:@"QuestionMemorizationLevelViewController" bundle:nil questionsArray:aQuestionsArray];
-  if (self != nil) {
-    self.isMemorizationLevelChosen = [NSNumber numberWithBool:NO];
-  }
-  
-  return self;
+    self = [super initWithNibName:@"QuestionMemorizationLevelViewController" bundle:nil questionsArray:aQuestionsArray];
+    if (self != nil) {
+        self.isMemorizationLevelChosen = [NSNumber numberWithBool:NO];
+    }
+    
+    return self;
+}
+
+
+#pragma mark - NavigationBar
+
+- (void)addNavigationBarNextQuestionButton{
+    // Do nothing.
+    // TODO Need to subclass the QuestionViewController and add only the “addNavigationBarNextQuestionButton“ to the class. Add the nextQuestionButton only to the QuestionViewController child class.
 }
 
 
@@ -39,9 +47,9 @@ static const int kDescriptionSection = 2;
  @remarks : <#(optional)#>
  */
 - (void)viewDidLoad{
-  [super viewDidLoad];
-  
-  [self.nextQuestionButton setUserInteractionEnabled:NO];
+    [super viewDidLoad];
+    
+    [self.nextQuestionButton setUserInteractionEnabled:NO];
 }
 
 
@@ -75,29 +83,29 @@ static NSString *kIsMemorizationLevelChosenKey = @"isMemorizationLevelChosen";
  It reduces the TableView height to make the "Memorizatoin level buttons" available.
  */
 - (void)updateUIForKeypath:(NSString *)keyPath {
-  [super updateUIForKeypath:keyPath];
-  
-  if ([keyPath isEqualToString:kIsMemorizationLevelChosenKey]){
-    if ([self.isMemorizationLevelChosen boolValue] == YES){
-      [self.nextQuestionButton setUserInteractionEnabled:YES];
-      
-      if ([self.shouldDisplayDescription boolValue] == YES){
-        [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:kDescriptionSection] withRowAnimation:UITableViewRowAnimationFade];
-        
-      }else if ([self.shouldDisplayAnswer boolValue] == YES){
-        [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:kAnswerSection] withRowAnimation:UITableViewRowAnimationFade];
-        
-      }
-    }else{
-      [self.nextQuestionButton setUserInteractionEnabled:NO];
-      
+    [super updateUIForKeypath:keyPath];
+    
+    if ([keyPath isEqualToString:kIsMemorizationLevelChosenKey]){
+        if ([self.isMemorizationLevelChosen boolValue] == YES){
+            [self.nextQuestionButton setUserInteractionEnabled:YES];
+            
+            if ([self.shouldDisplayDescription boolValue] == YES){
+                [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:kDescriptionSection] withRowAnimation:UITableViewRowAnimationFade];
+                
+            }else if ([self.shouldDisplayAnswer boolValue] == YES){
+                [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:kAnswerSection] withRowAnimation:UITableViewRowAnimationFade];
+                
+            }
+        }else{
+            [self.nextQuestionButton setUserInteractionEnabled:NO];
+            
+        }
     }
-  }
-  
-  if ([keyPath isEqualToString:kShouldDisplayAnswerKey]) {
-    if ([self.shouldDisplayAnswer boolValue] == YES && [self.shouldDisplayDescription boolValue] == NO) {
-      [self reduceTableViewHeight];
-    }
+    
+    if ([keyPath isEqualToString:kShouldDisplayAnswerKey]) {
+        if ([self.shouldDisplayAnswer boolValue] == YES && [self.shouldDisplayDescription boolValue] == NO) {
+            [self reduceTableViewHeight];
+        }
 	}
 }
 
@@ -111,15 +119,15 @@ static NSString *kIsMemorizationLevelChosenKey = @"isMemorizationLevelChosen";
  @remarks : It refresh the QuestionSetsViewController's TableView as well.
  */
 - (void)popQuestionSetsViewController{
-  [super popQuestionSetsViewController];
-  
-  UITabBarController *tabBarController = [APP_DELEGATE tabBarController];
-  NSArray *tabBarViewControllers = [tabBarController viewControllers];
-  UINavigationController *navController = [tabBarViewControllers objectAtIndex:0];
-  NSArray *navViewControllers = [navController viewControllers];
-  
-  QuestionSetsMemorizationLevelViewController *rootViewController = [navViewControllers objectAtIndex:navViewControllers.count - 1];
-  [rootViewController refreshView];
+    [super popQuestionSetsViewController];
+    
+    UITabBarController *tabBarController = [APP_DELEGATE tabBarController];
+    NSArray *tabBarViewControllers = [tabBarController viewControllers];
+    UINavigationController *navController = [tabBarViewControllers objectAtIndex:0];
+    NSArray *navViewControllers = [navController viewControllers];
+    
+    QuestionSetsMemorizationLevelViewController *rootViewController = [navViewControllers objectAtIndex:navViewControllers.count - 1];
+    [rootViewController refreshView];
 }
 
 /**
@@ -131,16 +139,16 @@ static NSString *kIsMemorizationLevelChosenKey = @"isMemorizationLevelChosen";
  If it's the last question from the QuestionSet, it pops to the RootViewController.
  */
 - (void)displayNextQuestion{
-  [super displayNextQuestion];
-  
-  if (self.currentQuestionIndex < [self.questionsArray count]){
-    // 1. Reset the button state to unselected.
-    [self removePreviousSelectedStateToButton];
-    // 2. As the TableView's height was reduced to present the "noting buttons" it has to be resized to its original size.
-    [self increaseTableViewHeight];
-  }
-  
-  self.isMemorizationLevelChosen = [NSNumber numberWithBool:NO];
+    [super displayNextQuestion];
+    
+    if (self.currentQuestionIndex < [self.questionsArray count]){
+        // 1. Reset the button state to unselected.
+        [self removePreviousSelectedStateToButton];
+        // 2. As the TableView's height was reduced to present the "noting buttons" it has to be resized to its original size.
+        [self increaseTableViewHeight];
+    }
+    
+    self.isMemorizationLevelChosen = [NSNumber numberWithBool:NO];
 }
 
 
@@ -150,191 +158,195 @@ static const float animationDuration = .4f;
 static const float boundsHeight = 105.f;
 
 - (void)reduceTableViewHeight{
-  @try {
-    self.tableView.contentMode = UIViewContentModeRedraw;
-    [UIView animateWithDuration:animationDuration animations:^{
-      CGRect theBounds = self.tableView.bounds;
-      CGPoint theCenter = self.tableView.center;
-      theBounds.size.height -= boundsHeight;
-      theCenter.y -= boundsHeight/2;
-      self.tableView.bounds = theBounds;
-      self.tableView.center = theCenter;
-    }];
-    
-  }
-  @catch (NSException *exception) {
-    OLLogDebug(@"%@", exception.reason);
-  }
-  @finally {
-    
-  }
+    @try {
+        self.tableView.contentMode = UIViewContentModeRedraw;
+        [UIView animateWithDuration:animationDuration animations:^{
+            CGRect theBounds = self.tableView.bounds;
+            CGPoint theCenter = self.tableView.center;
+            theBounds.size.height -= boundsHeight;
+            theCenter.y -= boundsHeight/2;
+            self.tableView.bounds = theBounds;
+            self.tableView.center = theCenter;
+        }];
+        
+    }
+    @catch (NSException *exception) {
+        OLLogDebug(@"%@", exception.reason);
+    }
+    @finally {
+        
+    }
 }
 
 - (void)increaseTableViewHeight{
-  @try {
-    self.tableView.contentMode = UIViewContentModeRedraw;
-    [UIView animateWithDuration:animationDuration animations:^{
-      CGRect theBounds = self.tableView.bounds;
-      CGPoint theCenter = self.tableView.center;
-      theBounds.size.height += boundsHeight;
-      theCenter.y += boundsHeight/2;
-      self.tableView.bounds = theBounds;
-      self.tableView.center = theCenter;
-    }];
-    
-  }
-  @catch (NSException *exception) {
-    OLLogDebug(@"%@", exception.reason);
-  }
-  @finally {
-    
-  }
+    @try {
+        self.tableView.contentMode = UIViewContentModeRedraw;
+        [UIView animateWithDuration:animationDuration animations:^{
+            CGRect theBounds = self.tableView.bounds;
+            CGPoint theCenter = self.tableView.center;
+            theBounds.size.height += boundsHeight;
+            theCenter.y += boundsHeight/2;
+            self.tableView.bounds = theBounds;
+            self.tableView.center = theCenter;
+        }];
+        
+    }
+    @catch (NSException *exception) {
+        OLLogDebug(@"%@", exception.reason);
+    }
+    @finally {
+        
+    }
 }
 
 
 #pragma mark - Footer
 
 - (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section{
-  NSString *footerTitle = @"";
-  switch (section) {
-    case kAnswerSection:
-      if ([self.shouldDisplayDescription boolValue] == NO) {
-        if ([self.shouldDisplayAnswer boolValue] == NO) {
-          footerTitle = @"Visualisez la réponse et appuyez sur \"Réponse\".";
-        }else{
-          if ([self.isMemorizationLevelChosen boolValue]) {
-            footerTitle = [NSString stringWithFormat:@"%d/5 pour cette question. \nPassez à la suivante en cliquant sur la plume en haut à droite.", self.memorizationLevel+1]; // As the "MemorizationLevel's Struct" starts at 0.
-          }else{
-            footerTitle = @"Appuyez sur la flèche si vous voulez en savoir plus ou choisissez une valeur ci-dessous.";
-          }
-        }
-      }
-      break;
-      
-    case kDescriptionSection:
-      if ([self.isMemorizationLevelChosen boolValue]) {
-        footerTitle = [NSString stringWithFormat:@"%d/5 pour cette question. \nPassez à la suivante en cliquant sur la plume en haut à droite.", self.memorizationLevel+1]; // As the "MemorizationLevel's Struct" starts at 0.
-      }else if ([self.shouldDisplayDescription boolValue]) {
-        footerTitle = @"Choisissez ci-dessous la valeur qui vous semble le plus à même de représenter votre niveau de mémorisation de cette question.";
-      }      break;
-      
-    default:
-      break;
-  }
-  
-  return footerTitle;
+    NSString *footerTitle = @"";
+    switch (section) {
+        case kAnswerSection:
+            if ([self.shouldDisplayDescription boolValue] == NO) {
+                if ([self.shouldDisplayAnswer boolValue] == NO) {
+                    footerTitle = @"Visualisez la réponse et appuyez sur \"Réponse\".";
+                }else{
+                    if ([self.isMemorizationLevelChosen boolValue]) {
+                        footerTitle = [NSString stringWithFormat:@"%d/5 pour cette question. \nPassez à la suivante en cliquant sur la plume en haut à droite.", self.memorizationLevel];
+                    }else{
+                        footerTitle = @"Appuyez sur la flèche si vous voulez en savoir plus ou choisissez une valeur ci-dessous.";
+                    }
+                }
+            }
+            break;
+            
+        case kDescriptionSection:
+            if ([self.isMemorizationLevelChosen boolValue]) {
+                footerTitle = [NSString stringWithFormat:@"%d/5 pour cette question. \nPassez à la suivante en cliquant sur la plume en haut à droite.", self.memorizationLevel];
+            }else if ([self.shouldDisplayDescription boolValue]) {
+                footerTitle = @"Choisissez ci-dessous la valeur qui vous semble le plus à même de représenter votre niveau de mémorisation de cette question.";
+            }      break;
+            
+        default:
+            break;
+    }
+    
+    return footerTitle;
 }
 
 
 #pragma mark - Memorization Level
 
 - (void)addDaysAccordingTo:(MemorizationLevel)aMemorizationLevel{
-  int daysToAdd = 0;
-  
-  switch (aMemorizationLevel) {
-    case MemorizationLevel1:
-      daysToAdd = 2;
-      break;
-      
-    case MemorizationLevel2:
-      daysToAdd = 2;
-      break;
-      
-    case MemorizationLevel3:
-      daysToAdd = 4;
-      break;
-      
-    case MemorizationLevel4:
-      daysToAdd = 10;
-      break;
-      
-    case MemorizationLevel5:
-      daysToAdd = 15;
-      break;
-      
-    default:
-      break;
-  }
-  Question *currentQuestion = [self.questionsArray objectAtIndex:self.currentQuestionIndex];
-  
-  // Set up date components
-  NSDateComponents *components = [[NSDateComponents alloc] init];
-  [components setDay:daysToAdd];
-  
-  // Create a calendar
-  NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-  
-  NSDate *nextPresentationDate = [gregorian dateByAddingComponents:components toDate:currentQuestion.nextPresentationDate options:0];
-  OLLogDebug(@"Question : %@;\nCurrent Presentation Date : %@;\nDays to add : %d;\nNext presentation date: %@", currentQuestion.statement, currentQuestion.nextPresentationDate, daysToAdd, nextPresentationDate);
-  currentQuestion.nextPresentationDate = nextPresentationDate;
+    int daysToAdd = 0;
+    
+    switch (aMemorizationLevel) {
+        case MemorizationLevel1:
+            daysToAdd = 2;
+            break;
+            
+        case MemorizationLevel2:
+            daysToAdd = 2;
+            break;
+            
+        case MemorizationLevel3:
+            daysToAdd = 4;
+            break;
+            
+        case MemorizationLevel4:
+            daysToAdd = 10;
+            break;
+            
+        case MemorizationLevel5:
+            daysToAdd = 15;
+            break;
+            
+        default:
+            break;
+    }
+    Question *currentQuestion = [self.questionsArray objectAtIndex:self.currentQuestionIndex];
+    [currentQuestion setUserLastMemorizationLevel:aMemorizationLevel];
+    
+    // Set up date components
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    [components setDay:daysToAdd];
+    
+    // Create a calendar
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    
+    NSDate *nextPresentationDate = [gregorian dateByAddingComponents:components toDate:currentQuestion.nextPresentationDate options:0];
+    OLLogDebug(@"Question : %@;\nCurrent Presentation Date : %@;\nDays to add : %d;\nNext presentation date: %@", currentQuestion.statement, currentQuestion.nextPresentationDate, daysToAdd, nextPresentationDate);
+    currentQuestion.nextPresentationDate = nextPresentationDate;
 }
 
 - (void)setSelectedState:(BOOL)isSelected toButton:(MemorizationLevel)memorizationLevel{
-  switch (memorizationLevel) {
-    case MemorizationLevel1:
-      [self.level1Button setSelected:isSelected];
-      break;
-      
-    case MemorizationLevel2:
-      [self.level2Button setSelected:isSelected];
-      break;
-      
-    case MemorizationLevel3:
-      [self.level3Button setSelected:isSelected];
-      break;
-      
-    case MemorizationLevel4:
-      [self.level4Button setSelected:isSelected];
-      break;
-      
-    case MemorizationLevel5:
-      [self.level5Button setSelected:isSelected];
-      break;
-      
-    default:
-      break;
-  }
+    switch (memorizationLevel) {
+        case MemorizationLevel1:
+            [self.level1Button setSelected:isSelected];
+            break;
+            
+        case MemorizationLevel2:
+            [self.level2Button setSelected:isSelected];
+            break;
+            
+        case MemorizationLevel3:
+            [self.level3Button setSelected:isSelected];
+            break;
+            
+        case MemorizationLevel4:
+            [self.level4Button setSelected:isSelected];
+            break;
+            
+        case MemorizationLevel5:
+            [self.level5Button setSelected:isSelected];
+            break;
+            
+        default:
+            break;
+    }
 }
 
 - (void)removePreviousSelectedStateToButton{
-  MemorizationLevel memorizationLevel = self.memorizationLevel;
-  [self setSelectedState:NO toButton:memorizationLevel];
+    MemorizationLevel memorizationLevel = self.memorizationLevel;
+    [self setSelectedState:NO toButton:memorizationLevel];
 }
 
 - (void)addSelectedStateToButton{
-  MemorizationLevel memorizationLevel = self.memorizationLevel;
-  [self setSelectedState:YES toButton:memorizationLevel];
+    MemorizationLevel memorizationLevel = self.memorizationLevel;
+    [self setSelectedState:YES toButton:memorizationLevel];
 }
 
 - (MemorizationLevel)memorisationLevelFromButton:(UIButton *)button{
-  MemorizationLevel memorizationLevel = MemorizationLevel1;
-  if (button == self.level1Button) {
-    memorizationLevel = MemorizationLevel1;
+    MemorizationLevel memorizationLevel = MemorizationLevel1;
+    if (button == self.level1Button) {
+        memorizationLevel = MemorizationLevel1;
+        
+    }else if (button == self.level2Button) {
+        memorizationLevel = MemorizationLevel2;
+        
+    }else if (button == self.level3Button) {
+        memorizationLevel = MemorizationLevel3;
+        
+    }else if (button == self.level4Button) {
+        memorizationLevel = MemorizationLevel4;
+        
+    }else if (button == self.level5Button) {
+        memorizationLevel = MemorizationLevel5;
+    }
     
-  }else if (button == self.level2Button) {
-    memorizationLevel = MemorizationLevel2;
-    
-  }else if (button == self.level3Button) {
-    memorizationLevel = MemorizationLevel3;
-    
-  }else if (button == self.level4Button) {
-    memorizationLevel = MemorizationLevel4;
-    
-  }else if (button == self.level5Button) {
-    memorizationLevel = MemorizationLevel5;
-  }
-  
-  return memorizationLevel;
+    return memorizationLevel;
 }
 
 - (IBAction)setMemorizationLevelFromButton:(UIButton *)sender {
-  [self removePreviousSelectedStateToButton];
-  self.memorizationLevel = [self memorisationLevelFromButton:sender];
-  [self addSelectedStateToButton];
-  
-  [self addDaysAccordingTo:self.memorizationLevel];
-  
-  self.isMemorizationLevelChosen = [NSNumber numberWithBool:YES];
+    [self removePreviousSelectedStateToButton];
+    self.memorizationLevel = [self memorisationLevelFromButton:sender];
+    [self addSelectedStateToButton];
+    
+    [self addDaysAccordingTo:self.memorizationLevel];
+    
+    self.isMemorizationLevelChosen = [NSNumber numberWithBool:YES];
+    
+    // Display the next question as the user selects his memorization level.
+    [self displayNextQuestion];
 }
 
 @end
