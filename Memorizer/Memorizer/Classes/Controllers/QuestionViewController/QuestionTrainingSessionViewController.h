61878629a5b39e34d47006d8dbde8842a51b45e6
Shosta <remilavedrine-git@yahoo.fr>
2013-04-19 22:59:31 +0200

    Added new QuestionTrainingSessionViewController that inherits from QuestionViewController.

---
+++ Memorizer/Memorizer/Classes/Controllers/QuestionViewController/QuestionTrainingSessionViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionTrainingSessionViewController.h
+//  Memorizer
+//
+//  Created by Rems on 19/04/13.
+//  Copyright (c) 2013 Rémi Lavedrine. All rights reserved.
+//
+
+#import "QuestionViewController.h"
+
+@interface QuestionTrainingSessionViewController : QuestionViewController
+
+@end
---
+++ Memorizer/Memorizer/Classes/Controllers/QuestionViewController/QuestionTrainingSessionViewController.m
@@ -0,0 +1,38 @@
+//
+//  QuestionTrainingSessionViewController.m
+//  Memorizer
+//
+//  Created by Rems on 19/04/13.
+//  Copyright (c) 2013 Rémi Lavedrine. All rights reserved.
+//
+
+#import "QuestionTrainingSessionViewController.h"
+
+@interface QuestionTrainingSessionViewController ()
+
+@end
+
+@implementation QuestionTrainingSessionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
